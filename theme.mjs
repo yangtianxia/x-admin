d0215f009835986454b1c1051b292c816bddca3e
yangtianxia <1304082170@qq.com>
2024-10-24 17:12:59 +0800

    feat 新增主题配置文件

---
+++ theme.mjs
@@ -0,0 +1,50 @@
+const primary = '#468ff5'
+const success = '#52c41a'
+const warning = '#eca311'
+const error = '#ff4d4f'
+const info = '#1677ff'
+
+export const palettes = {
+  primary,
+  success,
+  warning,
+  error,
+  info,
+  black: '#000000',
+  white: '#ffffff',
+  grey: [
+    '#f8f8f8',
+    '#efefef',
+    '#dfdfdf',
+    '#c6c6c6',
+    '#adadad',
+    '#949494',
+    '#626262',
+    '#494949',
+    '#303030'
+  ]
+}
+
+export const token = {
+  marginXXS: 4,
+  marginXS: 4,
+  marginSM: 8,
+  margin: 12,
+  marginMD: 16,
+  marginLG: 20,
+  marginXL: 24,
+  marginXXL: 32,
+  paddingXS: 4,
+  paddingSM: 8,
+  padding: 12,
+  paddingMD: 16,
+  paddingLG: 20,
+  paddingXL: 24,
+  borderRadius: 3,
+  wireframe: true,
+  colorPrimary: primary,
+  colorSuccess: success,
+  colorWarning: warning,
+  colorError: error,
+  colorInfo: info
+}
