5e390586430f66be07b16c6d32c41557f664c6a7
yangtianxia <1304082170@qq.com>
2024-10-25 15:53:45 +0800

    chore(views/login) 新增form页面组件

---
+++ src/views/login/components/form/index.tsx
@@ -0,0 +1,57 @@
+// Vue
+import { defineComponent } from 'vue'
+
+// Components
+import { Icon } from '@/components/icon'
+import {
+  Form,
+  FormItem,
+  Input,
+  Button
+} from 'ant-design-vue'
+
+// Style
+import style from './index.module.less'
+
+const [name, bem] = BEM('login-form', style)
+
+export default defineComponent({
+  name,
+  setup() {
+    return () => (
+      <div class={bem()}>
+        <div class={bem('title')}>登录 {import.meta.env.VITE_TITLE}</div>
+        <div class={bem('subtitle')}>{import.meta.env.VITE_DESCRIPTION}</div>
+        <Form>
+          <FormItem>
+            <Input
+              placeholder="用户名"
+              v-slots={{
+                prefix: () => (
+                  <Icon type="People" />
+                )
+              }}
+            />
+          </FormItem>
+          <FormItem>
+            <Input.Password
+              visibilityToggle
+              placeholder="登录密码"
+              v-slots={{
+                prefix: () => (
+                  <Icon type="Lock" />
+                )
+              }}
+            />
+          </FormItem>
+          <FormItem>
+            <Button
+              block
+              type="primary"
+            >登录</Button>
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
+})
