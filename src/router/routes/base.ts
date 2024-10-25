df5497e0efd0f67713bd5e3df2840cc20211783d
yangtianxia <1304082170@qq.com>
2024-10-25 15:54:42 +0800

    chore(router/routes) 移除REDIRECT_MAIN路由

--- src/router/routes/base.ts
+++ src/router/routes/base.ts
@@ -1,31 +1,10 @@
 import type { RouteRecordRaw } from 'vue-router'
-import { REDIRECT_ROUTE_NAME } from '../constant'
+import { NOT_FOUND_ROUTE_NAME } from '../constant'
 
 export const DEFAULT_LAYOUT = () => import('@/layout')
 
-export const REDIRECT_MAIN: RouteRecordRaw = {
-  path: '/redirect',
-  name: 'redirectWrapper',
-  component: DEFAULT_LAYOUT,
-  meta: {
-    requiresAuth: true,
-    hideInMenu: true
-  },
-  children: [
-    {
-      path: '/redirect/:path',
-      name: REDIRECT_ROUTE_NAME,
-      component: () => import('@/views/redirect'),
-      meta: {
-        requiresAuth: true,
-        hideInMenu: true
-      }
-    }
-  ]
-}
-
 export const NOT_FOUND_ROUTE: RouteRecordRaw = {
   path: '/:pathMatch(.*)*',
-  name: 'notFound',
+  name: NOT_FOUND_ROUTE_NAME,
   component: () => import('@/views/not-found')
 }
