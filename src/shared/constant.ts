58f0e16e0828b440659e73467241c693f84404c7
yangtianxia <1304082170@qq.com>
2024-10-25 16:13:34 +0800

    refactor(shared/constant) 使用createKey统一生成键值

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -1,11 +1,13 @@
+const createKey = <T extends string>(key: T) => `${import.meta.env.VITE_PREFIX}_${key}` as const
+
 /** 用户TOKEN */
-export const TOKEN_KEY = 'g-token'
+export const TOKEN_KEY = createKey('token')
 
 /** 本地存储LOCALE */
-export const LOCALE_KEY = 'g-locale'
+export const LOCALE_KEY = createKey('locale')
 
 /** 重定向URL */
-export const REDIRECT_URI = 'redirect_uri'
+export const REDIRECT_URI = createKey('redirect_uri')
 
 /** 重定向PARAMS */
-export const REDIRECT_PARAMS = 'redirect_params'
+export const REDIRECT_PARAMS = createKey('redirect_params')
