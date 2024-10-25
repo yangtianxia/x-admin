36b4f7ebfef002948797eb0fd644ce6411485384
yangtianxia <1304082170@qq.com>
2024-10-25 16:26:06 +0800

    refactor(api) 移除默认请求返回

--- src/api/app/menu.ts
+++ src/api/app/menu.ts
@@ -1,34 +1,6 @@
+import axios from '../with-axios'
+
+/** 菜单列表 */
 export function getMenuList() {
-  return Promise.resolve<any[]>([
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      meta: {
-        locale: 'menu.server.dashboard',
-        icon: 'dashboard-one',
-        requiresAuth: true,
-        order: 0,
-      },
-      children: [
-        {
-          path: 'workplace',
-          name: 'Workplace',
-          meta: {
-            locale: 'menu.server.workplace',
-            requiresAuth: true,
-          },
-        }
-      ],
-    },
-    {
-      path: 'https://baidu.com',
-      name: 'baiduWebsite',
-      meta: {
-        locale: 'menu.baiduWebsite',
-        icon: 'search',
-        requiresAuth: true,
-        order: 0
-      },
-    },
-  ])
+  return axios.get<any[]>('/menu/list')
 }
--- src/api/user/user-info.ts
+++ src/api/user/user-info.ts
@@ -1,18 +1,7 @@
 import type { UserState } from '@/store/modules/user/types'
+import axios from '../with-axios'
 
+/** 用户资料 */
 export function getUserInfo() {
-  return Promise.resolve<UserState>({
-    id: 1,
-    name: 'John Doe',
-    job: '前端开发',
-    avatar: 'https://example.com/avatar.jpg',
-    nickName: 'JD',
-    phone: '123-456-7890',
-    email: 'john.doe@example.com',
-    introduction: '我是一名前端开发工程师。',
-    location: '城市名称',
-    jobName: '职位名称',
-    certification: 12345,
-    role: 'user'
-  })
+  return axios.get<UserState>('/user/info')
 }
