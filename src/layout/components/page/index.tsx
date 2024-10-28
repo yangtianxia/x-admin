408f036c5fd826ff4efbb9121e9a1267601c041d
yangtianxia <1304082170@qq.com>
2024-10-28 08:58:10 +0800

    refactor(layout) 优化代码

--- src/layout/components/page/index.tsx
+++ src/layout/components/page/index.tsx
@@ -1,22 +1,28 @@
-import { defineComponent, Transition, KeepAlive, h } from 'vue'
+// Vue
+import {
+  defineComponent,
+  Transition,
+  KeepAlive,
+  h
+} from 'vue'
+
+// Common
 import { RouterView, type RouteLocationNormalized } from 'vue-router'
+
+interface RouterViewSlot  {
+  Component: any
+  route: RouteLocationNormalized
+}
 
 const [name] = BEM('page')
 
 export default defineComponent({
   name,
-
   setup() {
     return () => (
       <RouterView
         v-slots={{
-          default: ({
-            Component,
-            route
-          }: {
-            Component: any,
-            route: RouteLocationNormalized
-          }) => {
+          default: ({ Component, route }: RouterViewSlot) => {
             if (!Component) return
 
             Component.key = route.fullPath
