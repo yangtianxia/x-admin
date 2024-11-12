d57f46861f8f45fc4eff4d6bd29373a28c7a1d5e
yangtianxia <1304082170@qq.com>
2024-11-12 14:17:29 +0800

    feat(locale) 新增header和menu语言配置

---
+++ src/locale/lang/zh-CN/header.ts
@@ -0,0 +1,9 @@
+export default {
+  header: {
+    action: {
+      search: '搜索',
+      language: '语言',
+      locale: '切换为中文'
+    }
+  }
+}
---
+++ src/locale/lang/zh-CN/menu.ts
@@ -0,0 +1,9 @@
+export default {
+  menu: {
+    server: {
+      dashboard: '仪表盘',
+      workplace: '工作台'
+    },
+    baiduWebsite: '百度一下'
+  }
+}
