7a6a4241d4eb68e32be5dca664da4d4569d5c6f4
yangtianxia <1304082170@qq.com>
2024-11-12 11:30:52 +0800

    refactor(api) 配置新的api地址

--- src/api/user/user-info.ts
+++ src/api/user/user-info.ts
@@ -2,6 +2,6 @@ import type { UserState } from '@/store/modules/user/types'
 import axios from '../with-axios'
 
 /** 用户资料 */
-export function getUserInfo() {
+export async function getUserInfo() {
   return axios.get<UserState>('/user/info')
 }
