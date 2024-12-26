c72a3cfbb80730a9ad9220f17c0c6d85bc363b99
yangtianxia <1304082170@qq.com>
2024-12-26 16:02:31 +0800

    types(axios) 添加postForm、putForm、patchForm方法

--- types/axios.d.ts
+++ types/axios.d.ts
@@ -13,10 +13,14 @@ declare module 'axios' {
     request<T = any> (config: AxiosRequestConfig): Promise<T>
     get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
     delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
-    head<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
     post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
     put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
     patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+    postForm<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+    putForm<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+    patchForm<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+    head<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
+    options<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
   }
 }
 
