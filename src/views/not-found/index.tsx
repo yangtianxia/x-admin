205f1d667b96e964b597850d29522c9d34b2e871
yangtianxia <1304082170@qq.com>
2025-01-20 11:57:12 +0800

    chore(views) not-found页面添加返回按钮

--- src/views/not-found/index.tsx
+++ src/views/not-found/index.tsx
@@ -1,15 +1,29 @@
 // Vue
 import { defineComponent } from 'vue'
 
+// Common
+import { goBack } from '@/router'
+
 // Component
 import { Result } from '@/components/result'
+import { Icon } from '@/components/icon'
+import { Button } from 'ant-design-vue'
 
 export default defineComponent({
   name: 'NotFoundPage',
   setup() {
     return () => (
       <div class="h-screen flex flex-col items-center justify-center">
-        <Result status={404} />
+        <Result
+          status={404}
+          bottom={() => (
+            <Button
+              type="primary"
+              icon={<Icon type="Left"/>}
+              onClick={goBack}
+            >返回</Button>
+          )}
+        />
       </div>
     )
   }
