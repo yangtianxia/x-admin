ecc53a5a633202e048db9b9258840a4e0e5e1118
yangtianxia <1304082170@qq.com>
2025-02-11 09:28:09 +0800

    chore(views) 添加示例

--- src/views/example/upload/index.tsx
+++ src/views/example/upload/index.tsx
@@ -19,6 +19,12 @@ export default defineComponent({
           />
           <Upload
             v-model:fileList={fileList.value}
+            type="video"
+            accept="video/*"
+            urls={['https://drama-video-mp4.oss-cn-beijing.aliyuncs.com/zh-cn/%E6%80%BB%E8%A3%81%E8%80%81%E5%A9%86/%E6%80%BB%E8%A3%81%E8%80%81%E5%A9%86-%E7%AC%AC3%E9%9B%86.mp4']}
+          />
+          <Upload
+            v-model:fileList={fileList.value}
             type="file"
             urls={['https://gips0.baidu.com/it/u=3093851010,2626068568&fm=3042&app=3042&f=JPEG&wm=1,huayi,0,0,13,9&wmo=0,0&w=1024&h=1024']}
           />
