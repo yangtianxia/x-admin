94191f5f19ba9916d2d474c38b19ece71efe6fcb
yangtianxia <1304082170@qq.com>
2025-02-19 10:45:01 +0800

    chore(views) 调整页面

--- src/views/dashboard/workplace/index.tsx
+++ src/views/dashboard/workplace/index.tsx
@@ -9,13 +9,10 @@ export default defineComponent({
   setup () {
     return () => (
       <Scaffold loading={false}>
-        <Scaffold.Body
-          flex
-          shrink
-        >
+        <Scaffold.Body flex>
           <div class="flex-1 flex justify-center items-center flex-col">
             <div>
-              <h5 class="text-h5 text-right">你好。</h5>
+              <h5 class="text-h5 text-right text-primary/85">你好。</h5>
               <h1 class="text text-h1">x-admin</h1>
               <h5 class="text-h5">Hello.</h5>
             </div>
